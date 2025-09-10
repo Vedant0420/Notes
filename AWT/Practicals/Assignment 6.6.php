cc59acc9bd5b5c49fa289681d9134714c70c12d5
Vedant0420 <vedantkhedkar007@gmail.com>
2025-09-10 16:37:46 +0530

    AWT  UPDATED

--- AWT/Practicals/Assignment 6.6.php
+++ AWT/Practicals/Assignment 6.6.php
@@ -1,7 +1,7 @@
 <!-- Write a PHP code to accept employee name and joining date of employee in dd-mm-yyyy format and display his/her joining month -->
 
 <?php
-$name = "Om Mahadik";
+$name = "Vedant";
 $join_date = "14-09-2023";
 $date = DateTime::createFromFormat('d-m-Y', $join_date);
 $month = $date->format('F');
