6cfc39ad3a2beb6007528da30e0ad84dc62e0860
Vedant0420 <vedantkhedkar007@gmail.com>
2025-09-23 14:13:00 +0530

    AWT

---
+++ AWT/Notes/phpnotes.php
@@ -0,0 +1,31 @@
+<?php
+
+// echo statement to print data
+echo "Hello world<br>"; // echo statement
+// Can also be done like this: echo("Hello World");
+
+// variables - starts with $, [Local and Global Variables]
+$num1 = 20;
+echo "$num1<br>"; // will call 20 and print 20
+
+// --------------------------------------
+// Data types
+// --------------------------------------
+$num        = 10;       // Integer
+$percentage = 86.55;    // Float
+$name       = "MIT";    // String
+$a          = 'P';      // Character
+$flag       = true;     // Boolean
+/*  
+Array
+Object
+NULL
+*/
+
+// --------------------------------------
+// Constant
+// --------------------------------------
+define("PI", 3.142);
+echo PI . "<br>";
+
+?>
